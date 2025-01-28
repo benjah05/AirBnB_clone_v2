6ca31e1a4c52198ae519b47d3188381acc58cb76
benjah05 <senzelebenjamine@gmail.com>
2025-01-28 00:48:01 -0800

    compress file

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,8 +9,8 @@ def do_pack():
     time_string = time.strftime("%Y%m%d%H%M%S")
     try:
         local("mkdir -p versions")
-        local("tar -cvzf /versions/web_static_{}.tgz /web_static".
+        local("tar -cvzf versions/web_static_{}.tgz web_static".
               format(time_string))
-        return ("/versions/web_static_{}.tgz".format(time_string))
+        return ("versions/web_static_{}.tgz".format(time_string))
     except:
         return None
