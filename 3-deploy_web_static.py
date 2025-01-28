8140dbfebb71f6bcfefb6f90f313ccd9a0c47347
benjah05 <senzelebenjamine@gmail.com>
2025-01-28 10:29:54 -0800

    full deployment

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -11,9 +11,12 @@ def do_pack():
     """compress before sending"""
     time_string = time.strftime("%Y%m%d%H%M%S")
     try:
-        local("mkdir -p versions")
-        local("tar -cvzf /versions/web_static_{}.tgz /web_static".
-              format(time_string))
+        if os.path.isdir("versions") is False:
+            if local("mkdir -p versions").failed is True:
+                return None
+        if local("tar -cvzf /versions/web_static_{}.tgz /web_static".
+                 format(time_string)).failed is True:
+            return None
         return ("/versions/web_static_{}.tgz".format(time_string))
     except:
         return None
@@ -28,14 +31,23 @@ def do_deploy(archive_path):
         archive_filename = archive_path.split("/")[-1]
         new_path = ("/data/web_static/releases/" + archive_filename.
                     split(".")[0])
-        run("sudo mkdir -p {}".format(new_path))
-        run("sudo tar -xzf /tmp/{} -C {}".format(archive_filename, new_path))
-        run("sudo rm /tmp/{}".format(archive_filename))
-        run("sudo mv {}/web_static/* {}/".format(new_path, new_path))
-        run("sudo rm -rf {}/web_static".format(new_path))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s {} /data/web_static/current".format(new_path))
-        run("sudo cp {}/web_static/hbnb_static/0-index.html /data/web_static/current/")
+        if run("sudo mkdir -p {}".format(new_path)).failed is True:
+            return False
+        if run("sudo tar -xzf /tmp/{} -C {}".
+                format(archive_filename, new_path)).failed is True:
+            return False
+        if run("sudo rm /tmp/{}".format(archive_filename)).failed is True:
+            return False
+        if run("sudo mv {}/web_static/* {}/".
+                format(new_path, new_path)).failed is True:
+            return False
+        if run("sudo rm -rf {}/web_static".format(new_path)).failed is True:
+            return False
+        if run("sudo rm -rf /data/web_static/current").failed is True:
+            return False
+        if run("sudo ln -s {} /data/web_static/current".
+                format(new_path)).failed is True:
+            return False
         return True
     except:
         return False
